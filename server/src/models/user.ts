021a5041595e2cdb0d14ff135995fff15b418fa6
MickaelBoisDev <mickaelbois.dev@gmail.com>
2023-08-28 15:55:39 +0200

    first commit

---
+++ server/src/models/user.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import { UserDocument } from "../types/user.interface";
+import validator from "validator";
+import bcryptjs from "bcryptjs";
+
+const userSchema = new Schema<UserDocument>(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is require"],
+      validate: [validator.isEmail, "invalid email"],
+      createIndexes: { unique: true },
+    },
+    username: {
+      type: String,
+      required: [true, "Username is require"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is require"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+// Hash password on save / update => isolated logic
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcryptjs.genSalt(20);
+    this.password = await bcryptjs.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err as Error);
+  }
+});
+
+userSchema.methods.validatePassword = function (password: string) {
+  return bcryptjs.compare(password, this.password);
+};
+
+export default model<UserDocument>("User", userSchema);
