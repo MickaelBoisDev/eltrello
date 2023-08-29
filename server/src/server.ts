03d30b8e5b82752e03db3e516c05031d2f30a522
MickaelBoisDev <mickaelbois.dev@gmail.com>
2023-08-29 16:30:08 +0200

    setup CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,19 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import * as usersController from "./controllers/users";
 import bodyParser from "body-parser";
-import authMiddleWares from './middlewares/auth'
+import authMiddleWares from "./middlewares/auth";
+import cors from "cors";
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("API is UP");
+  res.send("API is UP");
 });
 
 app.post("/api/users", usersController.register);
@@ -21,12 +24,12 @@ app.post("/api/users/login", usersController.login);
 app.get("/api/user", authMiddleWares, usersController.currentUser);
 
 io.on("connection", () => {
-    console.log("connect");
+  console.log("connect");
 });
 
 mongoose.connect("mongodb://localhost:27017/eltrello").then(() => {
-    console.log("connectd to mongodb");
-    httpServer.listen(4001, () => {
-        console.log(`API is listening on port 4001`);
-    });
+  console.log("connectd to mongodb");
+  httpServer.listen(4001, () => {
+    console.log(`API is listening on port 4001`);
+  });
 });
